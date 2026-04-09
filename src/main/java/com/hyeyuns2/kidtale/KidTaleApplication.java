2af6087e1d4fcb9f786a7c82a955e7572892b2e6
S2hyeyunS2 <hhyeyun22@gmail.com>
2026-04-09 22:05:56 +0900

    init: spring boot project setup

---
+++ src/main/java/com/hyeyuns2/kidtale/KidTaleApplication.java
@@ -0,0 +1,13 @@
+package com.hyeyuns2.kidtale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidTaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidTaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyeyuns2/kidtale/KidTaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeyuns2.kidtale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidTaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
