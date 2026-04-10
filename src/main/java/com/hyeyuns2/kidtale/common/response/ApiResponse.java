a97984d8c88f96c39e62a772178034b91d1393b5
S2hyeyunS2 <hhyeyun22@gmail.com>
2026-04-10 16:18:54 +0900

    feat: ApiResponse 파일 생성

---
+++ src/main/java/com/hyeyuns2/kidtale/common/response/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.hyeyuns2.kidtale.common.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+    private final boolean success;
+    private final String message;
+    private final T data;
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(true, "요청이 성공했습니다.", data);
+    }
+
+    public static <T> ApiResponse<T> ok(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+    public static <T> ApiResponse<T> fail(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+}
