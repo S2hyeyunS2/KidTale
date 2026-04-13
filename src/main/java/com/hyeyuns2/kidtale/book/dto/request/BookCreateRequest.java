ee1c4a9a316c439fc8de9b04e764f30e93f5861f
김혜윤 <hhyeyun22@gmail.com>
2026-04-13 18:21:01 +0900

    fix: Gemini 호출 안정화 및 템플릿 선택 제거 후 책 생성 로직 개선 (#16)

--- src/main/java/com/hyeyuns2/kidtale/book/dto/request/BookCreateRequest.java
+++ src/main/java/com/hyeyuns2/kidtale/book/dto/request/BookCreateRequest.java
@@ -1,16 +1,9 @@
 package com.hyeyuns2.kidtale.book.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record BookCreateRequest(
 
         @NotNull(message = "동화 ID를 입력해주세요.")
-        Long storyId,
-
-        @NotBlank(message = "표지 템플릿 UID를 선택해주세요.")
-        String coverTemplateUid,
-
-        @NotBlank(message = "내지 템플릿 UID를 선택해주세요.")
-        String contentTemplateUid
+        Long storyId
 ) {}
