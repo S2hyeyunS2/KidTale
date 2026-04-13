e5abe041898ebcf775807de18985fab256e615de
S2hyeyunS2 <hhyeyun22@gmail.com>
2026-04-13 15:06:30 +0900

    feat: Book 생성 과정 ErrorCode 추가

--- src/main/java/com/hyeyuns2/kidtale/common/exception/ErrorCode.java
+++ src/main/java/com/hyeyuns2/kidtale/common/exception/ErrorCode.java
@@ -16,9 +16,11 @@ public enum ErrorCode {
     STORY_NOT_FOUND(HttpStatus.NOT_FOUND, "STORY_NOT_FOUND", "동화를 찾을 수 없습니다."),
     STORY_GENERATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "STORY_GENERATION_FAILED", "동화 생성에 실패했습니다."),
 
-    // SweetBook
+    // SweetBook / Book
     SWEETBOOK_API_ERROR(HttpStatus.BAD_GATEWAY, "SWEETBOOK_API_ERROR", "책 생성 중 오류가 발생했습니다."),
     BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "BOOK_NOT_FOUND", "책을 찾을 수 없습니다."),
+    BOOK_ALREADY_CREATED(HttpStatus.BAD_REQUEST, "BOOK_ALREADY_CREATED", "이미 책이 생성된 동화입니다."),
+    SWEETBOOK_NOT_CONFIGURED(HttpStatus.INTERNAL_SERVER_ERROR, "SWEETBOOK_NOT_CONFIGURED", "SweetBook 템플릿 설정이 필요합니다."),
 
     // Order
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER_NOT_FOUND", "주문을 찾을 수 없습니다."),
